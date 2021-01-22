656aeb1fee305a6292a20c47e48bb64bff14e306
oit17 <pavanaindra@mail.ru>
2021-01-22 15:46:28 +0300

    1

---
+++ src/Server.java
@@ -0,0 +1,36 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws IOException{
+        try (ServerSocket server=new ServerSocket(3345)){
+            Socket client=server.accept();
+            System.out.println("Соединение разрешено");
+            DataOutputStream out =new DataOutputStream(client.getOutputStream());
+            System.out.println("Поток записи создан");
+            DataInputStream input =new DataInputStream(client.getInputStream());
+            System.out.println("поток чтения создан");
+
+            while(!client.isClosed()){
+                System.out.println("сервер считывает");
+                String entry=input.readUTF();
+                System.out.println("прочитано: "+entry);
+
+                if(entry.equalsIgnoreCase(" name")){
+                    out.writeUTF("something "+entry);
+                    out.flush();
+                    input.close();
+                    out.close();
+                    client.close();
+                    break;
+                }
+            }
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
