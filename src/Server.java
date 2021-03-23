b490d4e15fb609f1351b643dc5f452733ccfaf34
oit17 <pavanaindra@mail.ru>
2021-03-23 15:11:44 +0300

    1

--- src/Server.java
+++ src/Server.java
@@ -7,37 +7,37 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 
 public class Server {
-    public static void main(String[] args) throws IOException{
-        try (ServerSocket server=new ServerSocket(3345)){
-            Socket client=server.accept();
-            System.out.println("Соединение разрешено");
-            DataOutputStream out =new DataOutputStream(client.getOutputStream());
-            System.out.println("Поток записи создан");
-            DataInputStream input =new DataInputStream(client.getInputStream());
-            System.out.println("поток чтения создан");
+    public static void main(String[] args) throws IOException {
+        if (1 == 1) {
+            try (ServerSocket server = new ServerSocket(3345)) {
+                Socket client = server.accept();
+                System.out.println("Соединение разрешено");
+                DataOutputStream out = new DataOutputStream(client.getOutputStream());
+                System.out.println("Поток записи создан");
+                DataInputStream input = new DataInputStream(client.getInputStream());
+                System.out.println("поток чтения создан");
 
-            while(/*!client.isClosed()*/1==1){
-                System.out.println("сервер считывает");
-                String entry=input.readUTF();
-                System.out.println("прочитано: "+entry);
-                BufferedWriter bwrtr=new BufferedWriter(new FileWriter("D:\\list.txt"));
-                bwrtr.write(entry);
-                bwrtr.close();
+                while (/*!client.isClosed()*/1 == 1) {
+                    System.out.println("сервер считывает");
+                    String entry = input.readUTF();
+                    System.out.println("прочитано: " + entry);
+                    BufferedWriter bwrtr = new BufferedWriter(new FileWriter("D:\\list.txt", true));
+                    bwrtr.write(" " + entry + "\r\n");
+                    bwrtr.close();
 
 
-                if(entry.equalsIgnoreCase(" name")){
-                    out.writeUTF("something "+entry);
-                    out.flush();
-                    input.close();
-                    out.close();
-                    client.close();
-                    break;
+                    if (entry.equalsIgnoreCase(" name")) {
+                        out.writeUTF("something " + entry);
+                        out.flush();
+                        input.close();
+                        out.close();
+                        //client.close();
+                        break;
+                    }
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        }
-
-        catch(IOException e){
-            e.printStackTrace();
         }
     }
 }
